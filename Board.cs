3657d0e4fbaf454f99aaf05e8bf83e0ed2b554f2
aaron kennis <aaronkennis@hotmail.com>
2019-05-22 10:38:14 +0200

    Raster toegevoegd

---
+++ Board.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    class Board
+    {
+        List<List<Cell>> cells = new List<List<Cell>>();
+
+        public Board()
+        {
+            for (int x = 0; x < 23; x++)
+            {
+                cells.Add(new List<Cell>());
+
+                for (int y = 0; y < 19; y++)
+                {
+                    cells[x].Add(new Cell(x, y));
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            cells.ForEach(
+                column => column.ForEach(
+                    cell => cell.Draw()
+                )
+            );
+        }
+
+        public static Vector2 getScreenPos(int x, int y)
+        {
+            return new Vector2(-1.2f + x * 0.111f, -1.0f + 0.111f / 2 + y * 0.111f);
+        }
+    }
+}
---
+++ Cell.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    public class Cell : Support.Texture
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Cell(int x, int y) : base("grass", new Vector2(), new Vector2(0.111f))
+        {
+            X = x;
+            Y = y;
+
+            Position = Board.getScreenPos(x, y);
+        }
+    }
+}
